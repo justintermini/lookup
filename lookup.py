0d9f52a74ede1ee492b633a68d9cba01117d3c25
Justin Termini <justintermini@gmail.com>
2020-10-14 16:52:42 -0400

    Playing with code

--- lookup.py
+++ lookup.py
@@ -1,1 +1,18 @@
+def input_file():
+    """Read addresses from a file into a dictionary."""
+    dict = {}
+    # Open address file in read format
+    f = open("address.txt", 'r')
+
+    while True:
+        line = f.readline()
+        
+        
+
+    #return dict
+
+input_file()
+
+
+
 
