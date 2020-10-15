5e601f6c772896f728baac916e99c62e193d25d8
Justin Termini <justintermini@gmail.com>
2020-10-15 10:37:02 -0400

    Added input_file function

--- lookup.py
+++ lookup.py
@@ -1,18 +1,32 @@
+import sys
+
 def input_file():
     """Read addresses from a file into a dictionary."""
-    dict = {}
-    # Open address file in read format
-    f = open("address.txt", 'r')
+    line_list = []
+    address_dict = {}
+
+    try:
+        f = open("address.txt", 'r')
+    except FileNotFoundError:
+        print("error: must use a valid filename.")
+        sys.exit(1)
 
     while True:
         line = f.readline()
-        
-        
+        if line == "":
+            break
+        line_list.append(line)
 
-    #return dict
+    for item in line_list:
+        values = item.split(",")
+        address_dict[values[0]] = (values[1], values[2], values[3], 
+                                  values[4], values[5].strip())
 
-input_file()
+    return address_dict
 
+address_file = input_file()
+
+print(address_file)
 
 
 
